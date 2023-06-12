63c7655bbac21ecba3383493cd2ae077419ec8b5
Marcin <marcin.t.kozak@gmail.com>
2023-06-12 21:42:27 +0100

    tasks done

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+
+        double money = 0;
+
+        if (salary <= 10000) {
+            money = salary - (salary * 0.15);
+        }
+        if (salary <= 20000) {
+            money = salary - (salary * 0.18);
+        }
+        if (salary > 20000) {
+            money = salary - (salary * 0.20);
+        }
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
